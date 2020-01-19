10333ab0b3bcb97a5484e4e36bd01e0632008cc9
Elliot Sasson <esasson@uci.edu>
2020-01-18 20:28:34 -0800

    Initial Commit

---
+++ flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixter
+//
+//  Created by Elliot Sasson on 1/18/20.
+//  Copyright © 2020 elliotsasson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
